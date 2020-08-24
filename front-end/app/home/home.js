74582b32ebbf1c9ea24e440bce1537ca3f31be76
Lucas Boz <lucas_boz@hotmail.com>
2020-08-23 22:23:17 -0300

    Ajustes em back-end e mapa

--- front-end/app/home/home.js
+++ front-end/app/home/home.js
@@ -43,16 +43,13 @@ angular.module('myApp.home', ['ngRoute'])
     /**
      * MAP INIT
      */
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([-27, -50], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
-
+ 
 
 
     /**
